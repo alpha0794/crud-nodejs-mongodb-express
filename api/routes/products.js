6788647e4e23cab02ee442fd7cf26d7ea83e974b
alpha0794 <uiuxdijain@gmail.com>
2021-05-29 15:42:33 +0530

    version change.
    version change.

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const lib = require('library-01');
+
 var dataBase = require('../connections/mongodb');
 
 router.get('/', (req, res, next) => {
@@ -9,37 +11,39 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-  const db = dataBase.get('crud-nodejs-mongodb-express');
-  if (req.body) {
-    var jsonObj = JSON.parse(JSON.stringify(req.body, null, 3));
-  } else {
-    var jsonObj = req;
-  }
-  var reqData = jsonObj.updateData;
-  var collectionName = jsonObj.collectionName;
-  var collection = db.collection(collectionName);
-  if (jsonObj) {
-    collection.insertOne(reqData, function (err, response) {
-      if (err) {
-        res.status(400).json({
-          status: '400',
-          message: 'Error creating product!',
-          error: err,
-        });
-      } else {
-        res.status(200).json({
-          status: '200',
-          message: 'Success creating product!',
-          data: response.ops,
-        });
-      }
-    });
-  } else {
-    res.status(400).json({
-      status: '400',
-      message: 'Please add body!',
-    });
-  }
+  let addNumber = lib.addNumbers(2, 3); // ().addNumber(2, 3);
+  console.log({ addNumber });
+  // const db = dataBase.get('crud-nodejs-mongodb-express');
+  // if (req.body) {
+  //   var jsonObj = JSON.parse(JSON.stringify(req.body, null, 3));
+  // } else {
+  //   var jsonObj = req;
+  // }
+  // var reqData = jsonObj.updateData;
+  // var collectionName = jsonObj.collectionName;
+  // var collection = db.collection(collectionName);
+  // if (jsonObj) {
+  //   collection.insertOne(reqData, function (err, response) {
+  //     if (err) {
+  //       res.status(400).json({
+  //         status: '400',
+  //         message: 'Error creating product!',
+  //         error: err,
+  //       });
+  //     } else {
+  //       res.status(200).json({
+  //         status: '200',
+  //         message: 'Success creating product!',
+  //         data: response.ops,
+  //       });
+  //     }
+  //   });
+  // } else {
+  //   res.status(400).json({
+  //     status: '400',
+  //     message: 'Please add body!',
+  //   });
+  // }
 });
 
 router.get('/:productId', (req, res, next) => {
